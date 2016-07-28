b8b9851cac0e99022a82e4790bbd4c94664072bb
developer <developer@entpy.com>
2016-07-28 18:39:40 +0200

    settings conf

--- entpy_v2/entpy_v2/settings.py
+++ entpy_v2/entpy_v2/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+GIT_PROJECT_NAME = "entpy_v2"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -120,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# loading local settings
+try:
+    LOCAL_SETTINGS
+except NameError:
+    try:
+        from local_settings import *
+    except ImportError:
+	pass
