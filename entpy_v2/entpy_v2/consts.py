e15dbf789353ff925dd96c1be62fb39c8d124fd3
developer <developer@entpy.com>
2016-10-06 15:00:00 +0200

    seo

--- entpy_v2/entpy_v2/consts.py
+++ entpy_v2/entpy_v2/consts.py
@@ -10,63 +10,75 @@ class project_constants(object):
     # object with SEO data (page title, page description, page keywords)
     seo_data = {
         "www_index" : {
-            "page_title" : None, # la home page non ha un titolo principale ma uno slider con diversi h1
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siti Web realmente utili basati su obiettivi - Acquisisci da oggi nuovi clienti.",
+            "page_title" : "Realizzazione Siti Internet", # la home page non ha un titolo principale ma uno slider con diversi h1
+            "description" : "Realizzazione Siti Internet Torino - Creazione Siti Internet Torino | Siti Web belli e utili, basati su obiettivi.",
+            "tag_title" : "Entpy | Realizzazione Siti Internet Torino - Creazione Siti Internet Torino",
             "keywords" : "",
         },
         "www_about" : {
             "page_title" : "Chi siamo",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Siamo di Torino, appassionati di informatica e specializzati nella realizzazione di Siti Web realmente utili, facciamo dell'informatica la nostra passione.",
+            "tag_title" : "Entpy Siti Internet | Chi Siamo",
             "keywords" : "",
         },
         "www_services" : {
             "page_title" : "Servizi",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Creiamo la tua presenza online completa al 100%: SEO, qualità, sicurezza e creatività.",
+            "description" : "Realizziamo siti web statici, siti web dinamici, e-commerce, ottimizzazione SEO, landing pages e campagne pubblicitarie ad-hoc.",
+            "tag_title" : "Entpy Siti Internet | I Nostri Servizi",
             "keywords" : "",
         },
         "www_static_site" : {
             "page_title" : "Siti statici",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Realizziamo il sito statico per la tua attività o impresa per offrirti maggiore visibilità nel mondo online.",
+            "tag_title" : "Entpy Siti Internet | Siti Statici",
             "keywords" : "",
         },
         "www_dynamic_site" : {
             "page_title" : "Siti dinamici",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Creiamo web dinamici per poterti offrire maggiori opportunità dal mondo web, come l'acquisizione di nuovi clienti e la promozione di prodotti/servizi.",
+            "tag_title" : "Entpy Siti Internet | Siti Dinamici",
             "keywords" : "",
         },
         "www_landing_pages" : {
             "page_title" : "Landing pages",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Le landing pages permettono di attuare singoli obiettivi, come per esempio l'acquisizione di nuovi clienti.",
+            "tag_title" : "Entpy Siti Internet | Landing Pages",
             "keywords" : "",
         },
         "www_seo" : {
             "page_title" : "SEO",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Con il SEO ci occupiamo di rendere il tuo sito web maggiormente reperibile dai motori di ricerca e di conseguenza dai tuoi potenziali clienti.",
+            "tag_title" : "Entpy Siti Internet | SEO",
             "keywords" : "",
         },
         "www_app" : {
             "page_title" : "Android e iPhone App",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Le App sono il futuro del campo informatico, possono portare maggior prestigio al tuo brand e offrirti nuovi metodi per rimanere in contatto con i tuoi clienti o trovarne di nuovi.",
+            "tag_title" : "Entpy Siti Internet | Android e iPhone App",
             "keywords" : "",
         },
         "www_advertising" : {
             "page_title" : "Pubblicità",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Siamo specializzati nella creazione di Siti Web futuristici realmente utili.",
+            "description" : "Creiamo campagne pubblicitare studiate appositamente attorno a te per portarti maggiore valore sul sito che realizzerai con noi o per quello che hai già.",
+            "tag_title" : "Entpy Siti Internet | Pubblicità",
             "keywords" : "",
         },
         "www_wizard" : {
             "page_title" : "Scelta guidata",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Abbiamo le idee giuste per te. Il preventivo è completamente gratuito.",
+            "description" : "Con il nostro wizard guidato ti aiutiamo a scegliere la soluzione più adatta alle tue esigenze, provalo ora.",
+            "tag_title" : "Entpy Siti Internet | Scelta guidata",
             "keywords" : "",
         },
         "www_our_offers" : {
             "page_title" : "Le nostre offerte",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Abbiamo le idee giuste per te. Il preventivo è completamente gratuito.",
+            "description" : "Scopri le offerte speciali dedicate a te, comunicaci il codice per poterne godere, ma affrettati...sono a tempo limitato!",
+            "tag_title" : "Entpy Siti Internet | Le nostre offerte",
             "keywords" : "",
         },
         "www_contact_us" : {
             "page_title" : "Contattaci",
-            "description" : "Realizzazione Siti Web Torino - Creazione Siti Web Torino | Abbiamo le idee giuste per te. Il preventivo è completamente gratuito.",
+            "description" : "Contattaci adesso, il preventivo è completamente gratuito. Inizia ad acquisire più visibilità e nuovi clienti con il tuo nuovo sito internet.",
+            "tag_title" : "Entpy Siti Internet | Contattaci",
             "keywords" : "",
         },
     }
